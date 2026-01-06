38601593d3c1b9b655ee351c8174ac3666cd615c
BiancaCirnu <bianca.georgiana.cirnu@gmail.com>
2026-01-06 17:34:27 +0200

    Added translations ro->en->ro

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TranslatorEvaluator
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
